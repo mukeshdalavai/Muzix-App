670c603b157ded26cd0288f7d1fac1738ad89713
Mukesh <mukeshdalavai@gmail.com>
2019-05-28 15:37:52 +0530

    Gateway Modifications

--- gatewayAPI/src/main/java/com/stackroute/gatewayAPI/GatewayApiApplication.java
+++ gatewayAPI/src/main/java/com/stackroute/gatewayAPI/GatewayApiApplication.java
@@ -1,9 +1,14 @@
 package com.stackroute.gatewayAPI;
 
+import com.stackroute.filters.ErrorFilter;
+import com.stackroute.filters.PostFilter;
+import com.stackroute.filters.PreFilter;
+import com.stackroute.filters.RouteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
@@ -14,5 +19,21 @@ public class GatewayApiApplication {
 		SpringApplication.run(GatewayApiApplication.class, args);
 	}
 
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
 }
 
