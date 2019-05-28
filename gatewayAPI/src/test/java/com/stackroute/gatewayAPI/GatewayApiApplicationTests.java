ebb246aaf58bc66320716ec8ad36e1c354150d4e
Mukesh <mukeshdalavai@gmail.com>
2019-05-28 15:31:25 +0530

    Gateway Modifications

--- gatewayAPI/src/test/java/com/stackroute/gatewayAPI/GatewayApiApplicationTests.java
+++ gatewayAPI/src/test/java/com/stackroute/gatewayAPI/GatewayApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.stackroute;
+package com.stackroute.gatewayAPI;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TrackServiceApplicationTests {
+public class GatewayApiApplicationTests {
 
 	@Test
 	public void contextLoads() {
